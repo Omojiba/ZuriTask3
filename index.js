b626ff187aacd75a4a7408be316395118c0bbf6b
Omojiba <tolu.omojiba@gmail.com>
2022-06-20 20:43:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var num1 = parseInt(prompt('Number 1: '))
+var opr = prompt('operator: ')
+var num2 = parseInt(prompt('Number 2: '))
+
+if (opr == '+')
+{ 
+    console.log(num1 + num2);
+}else if(opr == '-'){
+
+    console.log(num1 - num2);
+}else if(opr =='*'){
+
+    console.log(num1 * num2);
+}else if(opr == '/'){
+
+    console.log(num1 / num2);
+}
