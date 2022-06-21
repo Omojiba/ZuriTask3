990920ca15a77b8769b5180713b211a9a4c2ad13
Omojiba <tolu.omojiba@gmail.com>
2022-06-21 12:27:14 +0100

    .

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var num1 = parseInt(prompt('Number 1: '))
-var opr = prompt('operator: ')
-var num2 = parseInt(prompt('Number 2: '))
+for (let i= 0; i < 99999; i++){
+var num1 = parseInt(prompt('Number 1: '));
+var opr = prompt('operator: ');
+var num2 = parseInt(prompt('Number 2: '));
+
+var Message = 'Answer is';
 
 if (opr == '+')
 { 
@@ -17,6 +20,5 @@ if (opr == '+')
 }else if(opr == '/'){
 
     console.log(num1 / num2);
-    alert(num1 / num2)
-    al
-}
+    alert = (num1 / num2)
+}}
